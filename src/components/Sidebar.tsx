4e40fe53dabc990379715ef5953dcb9161dafbee
pebencze <petra.bncz@gmail.com>
2025-10-10 14:06:21 +0200

    refactor(Sidebar.tsx): collapse, expand, lucide icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,15 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
+import { TableOfContents } from "lucide-react"
+import {CircleX } from "lucide-react"
 
 
 function Sidebar(){
     const sidebarRef = useRef<HTMLDivElement>(null)
     const contentRef = useRef<HTMLDivElement>(null)
     const isBeingResizedRef = useRef<boolean>(false)
+
+    const [isBeingReset, setIsBeingReset] = useState(false)
+    const [isBeingCollapsed, setIsBeingCollapsed] = useState(false)
 
     
     const onDragResize = (event: React.MouseEvent<HTMLDivElement>) => {
@@ -37,25 +42,49 @@ function Sidebar(){
         document.removeEventListener("mousemove", onMouseDrag)
         document.removeEventListener("mouseup", onMouseRelease)
     }
+
+    const onResetWidth = () => {
+        if (sidebarRef.current && contentRef.current) {
+            setIsBeingReset(true)
+
+            sidebarRef.current.style.width = "240px"
+            contentRef.current.style.width = "240px"
+        }
+        setTimeout(() => setIsBeingReset(false), 500)
+
+    }
+
+    const onCollapse = () => {
+        if (sidebarRef.current && contentRef.current) {
+            setIsBeingCollapsed(true)
+
+            sidebarRef.current.style.width = "0px"
+            contentRef.current.style.width = "0px"
+        }
+        setTimeout(() => setIsBeingCollapsed(false), 500)
+    }
     
     return (
         <>
             <div 
                 ref={sidebarRef}
-                className="flex flex-col gap-4 overflow-y-scroll relative border-r bg-slate-100 p-4 z-10 w-[240px] group/sidebar"
+                className={`${
+                    (isBeingReset || isBeingCollapsed) && "transition-all duration-500 ease-out"
+                } flex flex-col gap-4 overflow-y-scroll relative border-r bg-slate-100 p-4 z-10 w-[240px] group/sidebar`}
             >
-                <button  
+                <button 
+                    onClick={onCollapse} 
                     className="opacity-0 transition-all group-hover/sidebar:opacity-100"
                 >
-                    Collapse
+                    <CircleX />
                 </button>
                 <div
                     onMouseDown={onDragResize} 
                     className="absolute top-0 right-0 cursor-ew-resize w-[2px] bg-slate-200 opacity-0 group-hover/sidebar:opacity-100 transition-all h-full"
                 ></div>
             </div>
-            <button className="">
-                Expand
+            <button onClick={onResetWidth} className="">
+                <TableOfContents />
             </button>
             {/* main content */}
             <div ref={contentRef} className="flex flex-col gap-4 overflow-y-scroll border-r bg-slate-100 p-4 z-10 w-[240px] h-screen">
